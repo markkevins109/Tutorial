9173690e3dd32147ec5b88bad148c818e1ce418b
markkevins109 <markkevin109@gmail.com>
2025-07-14 19:56:38 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Serve static files from /public
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Home route
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Socratic Tutor running at http://localhost:${PORT}`);
+});
